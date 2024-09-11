a744a90ee6a71c05691117a89f073ded3ae2f9f4
Takashi Hashida <hashida@clear-code.com>
2024-09-11 15:43:31 +0900

    run-test-server: add message of executed web server

--- tests/run-test-server/run-test-server.bat
+++ tests/run-test-server/run-test-server.bat
@@ -8,4 +8,5 @@ xcopy /y ..\..\local-web-server .
 rmdir /s /q .\web
 xcopy /y ..\..\dist .\web\
 xcopy /y .\configs .\web\configs\
+echo start web server: https://127.0.0.1:10041
 https_server.exe --root .\web
