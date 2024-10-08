be18d648475687f8a6ac31361ad8f84ff37fad88
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2024-10-08 14:54:39 +0900

    Cosmetic

--- src/web/attachments-confirmation.mjs
+++ src/web/attachments-confirmation.mjs
@@ -7,12 +7,14 @@ export class AttachmentsConfirmation {
   init(data) {
     const attachments = data.target.attachments || [];
     const unsafeFiles = data.config.unsafeFiles || [];
-    const unsafeAttachmentMatcher = unsafeFiles.length > 0 ?
-      new RegExp(unsafeFiles.map((pattern) => wildcardToRegexp(pattern)).join("|"), "i") :
-      null;
+    const unsafeAttachmentMatcher =
+      unsafeFiles.length > 0
+        ? new RegExp(unsafeFiles.map((pattern) => wildcardToRegexp(pattern)).join("|"), "i")
+        : null;
     for (const attachment of attachments) {
-      if (unsafeAttachmentMatcher && unsafeAttachmentMatcher.test(attachment.name))
+      if (unsafeAttachmentMatcher && unsafeAttachmentMatcher.test(attachment.name)) {
         this.unsafeAttachments.add(attachment);
+      }
       this.attachments.add(attachment);
     }
   }
