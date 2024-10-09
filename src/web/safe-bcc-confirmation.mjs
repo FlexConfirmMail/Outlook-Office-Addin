01458dba58141a9be31e9ef42b2f0ece64c90004
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2024-10-09 14:29:16 +0900

    Fix conrimation logic

--- src/web/safe-bcc-confirmation.mjs
+++ src/web/safe-bcc-confirmation.mjs
@@ -1,28 +1,27 @@
 export class SafeBccConfirmation {
-  shouldContirm = false;
-  domainsCount = 0;
+  shouldConfirm = false;
+  threshold = 0;
 
   init(data) {
     if (!data.config.common.SafeBccEnabled) {
       return;
     }
 
-    const threshold = data.config.common.SafeBccThreshold;
-    if (threshold < 1) {
+    this.threshold = data.config.common.SafeBccThreshold;
+    if (this.threshold < 1) {
       return;
     }
 
     const recipients = [...data.target.to, ...data.target.cc, ...data.target.bcc];
     const domains = new Set(recipients.map((recipient) => recipient.domain));
-    this.domainsCount = domains.size;
-    this.shouldContirm = this.domainsCount >= threshold;
+    this.shouldConfirm = domains.size >= this.threshold;
   }
 
   get warningConfirmationItems() {
-    if (!this.shouldContirm) {
+    if (!this.shouldConfirm) {
       return [];
     }
 
-    return [{ label: `[警告] To・Ccに${this.domainsCount}件以上のドメインが含まれています。` }];
+    return [{ label: `[警告] To・Ccに${this.threshold}件以上のドメインが含まれています。` }];
   }
 }
