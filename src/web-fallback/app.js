39f02f4b69dd7f9b9ba6a4d88f15f1d60b479b2c
Takashi Hashida <hashida@clear-code.com>
2025-02-05 10:55:37 +0900

    Add fallback modules
    The fallback modules simply allow all events.
    
    We can build fallback modules with `npm run build:fallback`.
    The build result is output to dist-fallback.

---
+++ src/web-fallback/app.js
@@ -0,0 +1,20 @@
+Office.onReady(() => {
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function onItemSend(event) {
+  event.completed({ allowEvent: true });
+}
+window.onItemSend = onItemSend;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function onNewMessageComposeCreated(event) {
+  event.completed();
+}
+window.onNewMessageComposeCreated = onNewMessageComposeCreated;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function onOpenSettingDialog(event) {
+  event.completed({ allowEvent: true });
+}
+window.onOpenSettingDialog = onOpenSettingDialog;
