b5aaf2259e592e178ad63ed18832065fbc80f474
Takashi Hashida <hashida@clear-code.com>
2025-03-07 13:23:05 +0900

    Add license text to files that can be distributed

--- src/web/setting.js
+++ src/web/setting.js
@@ -1,4 +1,10 @@
-// import { DesignToken } from '@microsoft/fast-foundation';
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2025 ClearCode Inc.
+*/
 import { ConfigLoader } from "./config-loader.mjs";
 import { L10n } from "./l10n.mjs";
 
