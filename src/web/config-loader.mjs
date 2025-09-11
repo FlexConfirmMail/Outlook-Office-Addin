639440c16a8e90efdefd4e1bdec9bd778f491283
Takashi Hashida <hashida@clear-code.com>
2025-09-11 15:21:04 +0900

    Create config.mjs
    Config specific functions and defines were moved to config.mjs from config-loader.mjs

--- src/web/config-loader.mjs
+++ src/web/config-loader.mjs
@@ -5,33 +5,10 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 Copyright (c) 2025 ClearCode Inc.
 */
+
+import { Config } from "./config.mjs";
+
 export class ConfigLoader {
-  static commonParamDefs = {
-    CountEnabled: "boolean",
-    CountAllowSkip: "boolean",
-    SafeBccEnabled: "boolean",
-    RequireCheckSubject: "boolean",
-    RequireCheckBody: "boolean",
-    MainSkipIfNoExt: "boolean",
-    AppointmentConfirmationEnabled: "boolean",
-    SafeNewDomainsEnabled: "boolean",
-    CountSeconds: "number",
-    SafeBccThreshold: "number",
-    SafeBccReconfirmationThreshold: "number",
-    DelayDeliveryEnabled: "boolean",
-    DelayDeliverySeconds: "number",
-    FixedParameters: "commaSeparatedValues",
-  };
-
-  static unsafeArraySectionDefs = ["WARNING", "BLOCK", "REWARNING"];
-
-  static defaultUnsafeConfigSection = "WARNING";
-
-  static unsafeBodiesParamDefs = {
-    Message: "text",
-    Keywords: "commaSeparatedValues",
-  };
-
   static DICTONARY_LINE_SPLITTER = /^([^=]+)=(.*)$/;
 
   static parseValue(paramDefs, key, valueStr) {
@@ -181,21 +158,21 @@ export class ConfigLoader {
   }
 
   static parseCommonConfig(str) {
-    const defaultSectionName = "Common";
-    const parsedDictionary = this.parseIni(str, this.commonParamDefs, defaultSectionName);
-    return parsedDictionary[defaultSectionName] || {};
+    const temporarySectionName = "Common";
+    const parsedDictionary = this.parseIni(str, Config.commonParamDefs, temporarySectionName);
+    return parsedDictionary[temporarySectionName] || {};
   }
 
   static parseUnsafeDomainsConfig(str) {
-    return this.parseSectionableArray(str, this.unsafeArraySectionDefs, this.defaultUnsafeConfigSection);
+    return this.parseSectionableArray(str, Config.unsafeArraySectionDefs, Config.defaultUnsafeDomainsConfigSection);
   }
 
   static parseUnsafeFilesConfig(str) {
-    return this.parseSectionableArray(str, this.unsafeArraySectionDefs, this.defaultUnsafeConfigSection);
+    return this.parseSectionableArray(str, Config.unsafeArraySectionDefs, Config.defaultUnsafeFilesConfigSection);
   }
 
   static parseUnsafeBodiesConfig(str) {
-    return this.parseIni(str, this.unsafeBodiesParamDefs, null);
+    return this.parseIni(str, Config.unsafeBodiesParamDefs, null);
   }
 
   static parseTrustedDomainsConfig(str) {
@@ -239,8 +216,7 @@ export class ConfigLoader {
       this.loadFileConfig(),
       this.loadUserConfig(),
     ]);
-    const effectiveFileConfig = this.merge(this.createDefaultConfig(), fileConfig);
-    const effectiveConfig = this.merge(effectiveFileConfig, userConfig);
+    const effectiveConfig = Config.createDefaultConfig().merge(fileConfig).merge(userConfig);
     return effectiveConfig;
   }
 
@@ -263,7 +239,7 @@ export class ConfigLoader {
     const unsafeFiles = this.parseUnsafeFilesConfig(unsafeFilesString);
     const unsafeBodies = this.parseUnsafeBodiesConfig(unsafeBodiesString);
     const common = this.parseCommonConfig(commonString);
-    return {
+    return new Config({
       trustedDomains,
       unsafeDomains,
       unsafeFiles,
@@ -274,7 +250,7 @@ export class ConfigLoader {
       unsafeFilesString,
       unsafeBodiesString,
       commonString,
-    };
+    });
   }
 
   /**
@@ -295,7 +271,7 @@ export class ConfigLoader {
     const unsafeFiles = this.parseUnsafeFilesConfig(unsafeFilesString);
     const unsafeBodies = this.parseUnsafeBodiesConfig(unsafeBodiesString);
     const common = this.parseCommonConfig(commonString);
-    return {
+    return new Config({
       common,
       trustedDomains,
       unsafeDomains,
@@ -306,216 +282,6 @@ export class ConfigLoader {
       unsafeDomainsString,
       unsafeFilesString,
       unsafeBodiesString,
-    };
-  }
-
-  static createDefaultConfig() {
-    return {
-      common: {
-        CountEnabled: true,
-        CountAllowSkip: true,
-        SafeBccEnabled: true,
-        RequireCheckSubject: false,
-        RequireCheckBody: false,
-        MainSkipIfNoExt: false,
-        AppointmentConfirmationEnabled: false,
-        SafeNewDomainsEnabled: true,
-        CountSeconds: 3,
-        SafeBccThreshold: 4,
-        SafeBccReconfirmationThreshold: 0,
-        DelayDeliveryEnabled: false,
-        DelayDeliverySeconds: 60,
-        FixedParameters: [],
-      },
-      trustedDomains: [],
-      unsafeDomains: {},
-      unsafeFiles: {},
-      unsafeBodies: {},
-      commonString: "",
-      trustedDomainsString: "",
-      unsafeDomainsString: "",
-      unsafeFilesString: "",
-      unsafeBodiesString: "",
-    };
-  }
-
-  static createEmptyConfig() {
-    return {
-      common: {},
-      trustedDomains: [],
-      unsafeDomains: {},
-      unsafeFiles: {},
-      unsafeBodies: {},
-      commonString: "",
-      trustedDomainsString: "",
-      unsafeDomainsString: "",
-      unsafeFilesString: "",
-      unsafeBodiesString: "",
-    };
-  }
-
-  static mergeUnsafeConfig(params, left, right) {
-    const result = {};
-    for (const param of params) {
-      const leftValue = left[param] || [];
-      const rightValue = right[param] || [];
-      const resultValue = leftValue.concat(rightValue);
-      if (resultValue.length == 0) {
-        continue;
-      }
-      result[param] = leftValue.concat(rightValue);
-    }
-    return result;
-  }
-
-  static merge(left, right) {
-    const fixedParametersSet = new Set(left.common.FixedParameters ?? []);
-    if (right.common.CountEnabled != null && !fixedParametersSet.has("CountEnabled")) {
-      left.common.CountEnabled = right.common.CountEnabled;
-    }
-    if (right.common.CountAllowSkip != null && !fixedParametersSet.has("CountAllowSkip")) {
-      left.common.CountAllowSkip = right.common.CountAllowSkip;
-    }
-    if (right.common.SafeBccEnabled != null && !fixedParametersSet.has("SafeBccEnabled")) {
-      left.common.SafeBccEnabled = right.common.SafeBccEnabled;
-    }
-    if (
-      right.common.RequireCheckSubject != null &&
-      !fixedParametersSet.has("RequireCheckSubject")
-    ) {
-      left.common.RequireCheckSubject = right.common.RequireCheckSubject;
-    }
-    if (right.common.RequireCheckBody != null && !fixedParametersSet.has("RequireCheckBody")) {
-      left.common.RequireCheckBody = right.common.RequireCheckBody;
-    }
-    if (right.common.MainSkipIfNoExt != null && !fixedParametersSet.has("MainSkipIfNoExt")) {
-      left.common.MainSkipIfNoExt = right.common.MainSkipIfNoExt;
-    }
-    if (
-      right.common.AppointmentConfirmationEnabled != null &&
-      !fixedParametersSet.has("AppointmentConfirmationEnabled")
-    ) {
-      left.common.AppointmentConfirmationEnabled = right.common.AppointmentConfirmationEnabled;
-    }
-    if (
-      right.common.SafeNewDomainsEnabled != null &&
-      !fixedParametersSet.has("SafeNewDomainsEnabled")
-    ) {
-      left.common.SafeNewDomainsEnabled = right.common.SafeNewDomainsEnabled;
-    }
-    if (right.common.CountSeconds != null && !fixedParametersSet.has("CountSeconds")) {
-      left.common.CountSeconds = right.common.CountSeconds;
-    }
-    if (right.common.SafeBccThreshold != null && !fixedParametersSet.has("SafeBccThreshold")) {
-      left.common.SafeBccThreshold = right.common.SafeBccThreshold;
-    }
-    if (
-      right.common.SafeBccReconfirmationThreshold != null &&
-      !fixedParametersSet.has("SafeBccReconfirmationThreshold")
-    ) {
-      left.common.SafeBccReconfirmationThreshold = right.common.SafeBccReconfirmationThreshold;
-    }
-    if (
-      right.common.DelayDeliveryEnabled != null &&
-      !fixedParametersSet.has("DelayDeliveryEnabled")
-    ) {
-      left.common.DelayDeliveryEnabled = right.common.DelayDeliveryEnabled;
-    }
-    if (
-      right.common.DelayDeliverySeconds != null &&
-      !fixedParametersSet.has("DelayDeliverySeconds")
-    ) {
-      left.common.DelayDeliverySeconds = right.common.DelayDeliverySeconds;
-    }
-    if (!fixedParametersSet.has("TrustedDomains")) {
-      left.trustedDomains = left.trustedDomains.concat(right.trustedDomains);
-      left.trustedDomainsString += "\n" + right.trustedDomainsString;
-      left.trustedDomainsString = left.trustedDomainsString.trim();
-    }
-    if (!fixedParametersSet.has("UnsafeDomains")) {
-      left.unsafeDomains = this.mergeUnsafeConfig(
-        this.unsafeArraySectionDefs,
-        left.unsafeDomains,
-        right.unsafeDomains
-      );
-      if (left.unsafeDomainsString && right.unsafeDomainsString) {
-        // We must add [WARNING] just before right string.
-        // We can ommit [WARNING] section declaration, so when right ommits [WARNING] section declaration,
-        // the right [WARNING] section may be in the wrong section after merged.
-        //
-        // If [WARNING] is not added:
-        //   left:
-        //     [BLOCK]
-        //     a@example.com
-        //   right:
-        //     b@example.com
-        //   merged:
-        //     [BLOCK]
-        //     a@example.com
-        //     b@example.com
-        //
-        // In this case, b@example.com is expected in [WARNING] but in [BLOCK].
-        //
-        // By adding [WARNING]:
-        //   left:
-        //     [BLOCK]
-        //     a@example.com
-        //   right:
-        //     b@example.com
-        //   merged:
-        //     [BLOCK]
-        //     a@example.com
-        //     [WARNING]
-        //     b@example.com
-        //
-        // In this case, b@example.com is in [WARNING] as expected.
-        left.unsafeDomainsString +=
-          `\n[${this.defaultUnsafeConfigSection}]\n` + right.unsafeDomainsString;
-      } else {
-        left.unsafeDomainsString += "\n" + right.unsafeDomainsString;
-      }
-      left.unsafeDomainsString = left.unsafeDomainsString.trim();
-    }
-    if (!fixedParametersSet.has("UnsafeFiles")) {
-      left.unsafeFiles = this.mergeUnsafeConfig(
-        this.unsafeArraySectionDefs,
-        left.unsafeFiles,
-        right.unsafeFiles
-      );
-      if (left.unsafeFilesString && right.unsafeFilesString) {
-        left.unsafeFilesString +=
-          `\n[${this.defaultUnsafeConfigSection}]\n` + right.unsafeFilesString;
-      } else {
-        left.unsafeFilesString += "\n" + right.unsafeFilesString;
-      }
-      left.unsafeFilesString = left.unsafeFilesString.trim();
-    }
-    if (!fixedParametersSet.has("UnsafeBodies")) {
-      const leftUnsafeBodies = left.unsafeBodies || {};
-      const rightUnsafeBodies = right.unsafeBodies || {};
-      // If there is the same section name in the left and the right, the section of right is used.
-      const mergedUnsafeBodies = Object.assign({}, leftUnsafeBodies, rightUnsafeBodies);
-      left.unsafeBodies = mergedUnsafeBodies;
-      // If there is the same section name in the left and the right, both of them are written
-      // in the unsafeBodiesString. As the config file specification, the later section overrides
-      // the previous, as the result, the section of right is used.
-      left.unsafeBodiesString += "\n" + right.unsafeBodiesString;
-      left.unsafeBodiesString = left.unsafeBodiesString.trim();
-    }
-    const rightFixedParametersSet = new Set(right.common.FixedParameters ?? []);
-    const newFixedParametersSet = new Set([...fixedParametersSet, ...rightFixedParametersSet]);
-    left.common.FixedParameters = [...newFixedParametersSet];
-    let commonString = "";
-    for (const [key, value] of Object.entries(left.common)) {
-      if (key === "FixedParameters") {
-        if (value.length > 0) {
-          commonString += `${key} = ${value.join(",")}\n`;
-        }
-      } else {
-        commonString += `${key} = ${value}\n`;
-      }
-    }
-    left.commonString = commonString.trim();
-    return left;
+    });
   }
 }
---
+++ src/web/config.mjs
@@ -0,0 +1,292 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2025 ClearCode Inc.
+*/
+export class Config {
+  common = {};
+  trustedDomains = [];
+  unsafeDomains = {};
+  unsafeFiles = {};
+  unsafeBodies = {};
+  commonString = "";
+  trustedDomainsString = "";
+  unsafeDomainsString = "";
+  unsafeFilesString = "";
+  unsafeBodiesString = "";
+
+  constructor({
+      common,
+      trustedDomains,
+      unsafeDomains,
+      unsafeFiles,
+      unsafeBodies,
+      commonString,
+      trustedDomainsString,
+      unsafeDomainsString,
+      unsafeFilesString,
+      unsafeBodiesString,
+    })
+  {
+      this.common = common;
+      this.trustedDomains = trustedDomains;
+      this.unsafeDomains = unsafeDomains;
+      this.unsafeFiles = unsafeFiles;
+      this.unsafeBodies = unsafeBodies;
+      this.commonString = commonString;
+      this.trustedDomainsString = trustedDomainsString;
+      this.unsafeDomainsString = unsafeDomainsString;
+      this.unsafeFilesString = unsafeFilesString;
+      this.unsafeBodiesString = unsafeBodiesString;
+  }
+
+  merge(other) {
+    const fixedParametersSet = new Set(this.common.FixedParameters ?? []);
+    if (other.common.CountEnabled != null && !fixedParametersSet.has("CountEnabled")) {
+      this.common.CountEnabled = other.common.CountEnabled;
+    }
+    if (other.common.CountAllowSkip != null && !fixedParametersSet.has("CountAllowSkip")) {
+      this.common.CountAllowSkip = other.common.CountAllowSkip;
+    }
+    if (other.common.SafeBccEnabled != null && !fixedParametersSet.has("SafeBccEnabled")) {
+      this.common.SafeBccEnabled = other.common.SafeBccEnabled;
+    }
+    if (
+      other.common.RequireCheckSubject != null &&
+      !fixedParametersSet.has("RequireCheckSubject")
+    ) {
+      this.common.RequireCheckSubject = other.common.RequireCheckSubject;
+    }
+    if (other.common.RequireCheckBody != null && !fixedParametersSet.has("RequireCheckBody")) {
+      this.common.RequireCheckBody = other.common.RequireCheckBody;
+    }
+    if (other.common.MainSkipIfNoExt != null && !fixedParametersSet.has("MainSkipIfNoExt")) {
+      this.common.MainSkipIfNoExt = other.common.MainSkipIfNoExt;
+    }
+    if (
+      other.common.AppointmentConfirmationEnabled != null &&
+      !fixedParametersSet.has("AppointmentConfirmationEnabled")
+    ) {
+      this.common.AppointmentConfirmationEnabled = other.common.AppointmentConfirmationEnabled;
+    }
+    if (
+      other.common.SafeNewDomainsEnabled != null &&
+      !fixedParametersSet.has("SafeNewDomainsEnabled")
+    ) {
+      this.common.SafeNewDomainsEnabled = other.common.SafeNewDomainsEnabled;
+    }
+    if (other.common.CountSeconds != null && !fixedParametersSet.has("CountSeconds")) {
+      this.common.CountSeconds = other.common.CountSeconds;
+    }
+    if (other.common.SafeBccThreshold != null && !fixedParametersSet.has("SafeBccThreshold")) {
+      this.common.SafeBccThreshold = other.common.SafeBccThreshold;
+    }
+    if (
+      other.common.SafeBccReconfirmationThreshold != null &&
+      !fixedParametersSet.has("SafeBccReconfirmationThreshold")
+    ) {
+      this.common.SafeBccReconfirmationThreshold = other.common.SafeBccReconfirmationThreshold;
+    }
+    if (
+      other.common.DelayDeliveryEnabled != null &&
+      !fixedParametersSet.has("DelayDeliveryEnabled")
+    ) {
+      this.common.DelayDeliveryEnabled = other.common.DelayDeliveryEnabled;
+    }
+    if (
+      other.common.DelayDeliverySeconds != null &&
+      !fixedParametersSet.has("DelayDeliverySeconds")
+    ) {
+      this.common.DelayDeliverySeconds = other.common.DelayDeliverySeconds;
+    }
+    if (!fixedParametersSet.has("TrustedDomains")) {
+      this.trustedDomains = this.trustedDomains.concat(other.trustedDomains);
+      this.trustedDomainsString += "\n" + other.trustedDomainsString;
+      this.trustedDomainsString = this.trustedDomainsString.trim();
+    }
+    if (!fixedParametersSet.has("UnsafeDomains")) {
+      this.unsafeDomains = Config.mergeSectionableArrayConfig(
+        Config.unsafeArraySectionDefs,
+        this.unsafeDomains,
+        other.unsafeDomains
+      );
+      if (this.unsafeDomainsString && other.unsafeDomainsString) {
+        // We must add [WARNING] just before right string.
+        // We can ommit [WARNING] section declaration, so when right ommits [WARNING] section declaration,
+        // the right [WARNING] section may be in the wrong section after merged.
+        //
+        // If [WARNING] is not added:
+        //   left:
+        //     [BLOCK]
+        //     a@example.com
+        //   right:
+        //     b@example.com
+        //   merged:
+        //     [BLOCK]
+        //     a@example.com
+        //     b@example.com
+        //
+        // In this case, b@example.com is expected in [WARNING] but in [BLOCK].
+        //
+        // By adding [WARNING]:
+        //   left:
+        //     [BLOCK]
+        //     a@example.com
+        //   right:
+        //     b@example.com
+        //   merged:
+        //     [BLOCK]
+        //     a@example.com
+        //     [WARNING]
+        //     b@example.com
+        //
+        // In this case, b@example.com is in [WARNING] as expected.
+        this.unsafeDomainsString +=
+          `\n[${Config.defaultUnsafeDomainsConfigSection}]\n` + other.unsafeDomainsString;
+      } else {
+        this.unsafeDomainsString += "\n" + other.unsafeDomainsString;
+      }
+      this.unsafeDomainsString = this.unsafeDomainsString.trim();
+    }
+    if (!fixedParametersSet.has("UnsafeFiles")) {
+      this.unsafeFiles = Config.mergeSectionableArrayConfig(
+        Config.unsafeArraySectionDefs,
+        this.unsafeFiles,
+        other.unsafeFiles
+      );
+      if (this.unsafeFilesString && other.unsafeFilesString) {
+        this.unsafeFilesString +=
+          `\n[${Config.defaultUnsafeFilesConfigSection}]\n` + other.unsafeFilesString;
+      } else {
+        this.unsafeFilesString += "\n" + other.unsafeFilesString;
+      }
+      this.unsafeFilesString = this.unsafeFilesString.trim();
+    }
+    if (!fixedParametersSet.has("UnsafeBodies")) {
+      const leftUnsafeBodies = this.unsafeBodies || {};
+      const rightUnsafeBodies = other.unsafeBodies || {};
+      // If there is the same section name in the left and the right, the section of right is used.
+      const mergedUnsafeBodies = Object.assign({}, leftUnsafeBodies, rightUnsafeBodies);
+      this.unsafeBodies = mergedUnsafeBodies;
+      // If there is the same section name in the left and the right, both of them are written
+      // in the unsafeBodiesString. As the config file specification, the later section overrides
+      // the previous, as the result, the section of right is used.
+      this.unsafeBodiesString += "\n" + other.unsafeBodiesString;
+      this.unsafeBodiesString = this.unsafeBodiesString.trim();
+    }
+    const rightFixedParametersSet = new Set(other.common.FixedParameters ?? []);
+    const newFixedParametersSet = new Set([...fixedParametersSet, ...rightFixedParametersSet]);
+    this.common.FixedParameters = [...newFixedParametersSet];
+    let commonString = "";
+    for (const [key, value] of Object.entries(this.common)) {
+      if (key === "FixedParameters") {
+        if (value.length > 0) {
+          commonString += `${key} = ${value.join(",")}\n`;
+        }
+      } else {
+        commonString += `${key} = ${value}\n`;
+      }
+    }
+    this.commonString = commonString.trim();
+    return this;
+  }
+
+  static commonParamDefs = {
+    CountEnabled: "boolean",
+    CountAllowSkip: "boolean",
+    SafeBccEnabled: "boolean",
+    RequireCheckSubject: "boolean",
+    RequireCheckBody: "boolean",
+    MainSkipIfNoExt: "boolean",
+    AppointmentConfirmationEnabled: "boolean",
+    SafeNewDomainsEnabled: "boolean",
+    CountSeconds: "number",
+    SafeBccThreshold: "number",
+    SafeBccReconfirmationThreshold: "number",
+    DelayDeliveryEnabled: "boolean",
+    DelayDeliverySeconds: "number",
+    FixedParameters: "commaSeparatedValues",
+  };
+  static unsafeBodiesParamDefs = {
+    Message: "text",
+    Keywords: "commaSeparatedValues",
+  };
+  static unsafeArraySectionDefs = ["WARNING", "BLOCK", "REWARNING"];
+  static defaultUnsafeDomainsConfigSection = "WARNING";
+  static defaultUnsafeFilesConfigSection = "WARNING";
+
+  static createDefaultConfig() {
+    return new Config({
+      common: {
+        CountEnabled: true,
+        CountAllowSkip: true,
+        SafeBccEnabled: true,
+        RequireCheckSubject: false,
+        RequireCheckBody: false,
+        MainSkipIfNoExt: false,
+        AppointmentConfirmationEnabled: false,
+        SafeNewDomainsEnabled: true,
+        CountSeconds: 3,
+        SafeBccThreshold: 4,
+        SafeBccReconfirmationThreshold: 0,
+        DelayDeliveryEnabled: false,
+        DelayDeliverySeconds: 60,
+        FixedParameters: [],
+      },
+      trustedDomains: [],
+      unsafeDomains: {},
+      unsafeFiles: {},
+      unsafeBodies: {},
+      commonString: "",
+      trustedDomainsString: "",
+      unsafeDomainsString: "",
+      unsafeFilesString: "",
+      unsafeBodiesString: "",
+    });
+  }
+
+  static createEmptyConfig() {
+    return new Config({
+      common: {},
+      trustedDomains: [],
+      unsafeDomains: {},
+      unsafeFiles: {},
+      unsafeBodies: {},
+      commonString: "",
+      trustedDomainsString: "",
+      unsafeDomainsString: "",
+      unsafeFilesString: "",
+      unsafeBodiesString: "",
+    });
+  }
+
+  static mergeSectionableArrayConfig(params, left, right) {
+    const result = {};
+    for (const param of params) {
+      const leftValue = left[param] || [];
+      const rightValue = right[param] || [];
+      const resultValue = leftValue.concat(rightValue);
+      if (resultValue.length == 0) {
+        continue;
+      }
+      result[param] = leftValue.concat(rightValue);
+    }
+    return result;
+  }
+
+  static serializeSectionableArray(config, sectionDefs) {
+    if (!config) {
+      return "";
+    }
+    let lines = [];
+    for (const sectionName of sectionDefs) {
+      if (config[sectionName] && config[sectionName].length > 0) {
+        lines.push(`[${sectionName}]`);
+        lines = lines.concat(config[sectionName]);
+      }
+    }
+    return lines.join("\n# ");
+  }
+}
