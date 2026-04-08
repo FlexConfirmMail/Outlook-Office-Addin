3392f0ebe9171e4b8a0c0abbebf5714153bb3888
Takashi Hashida <hashida@clear-code.com>
2026-04-08 17:43:34 +0900

    Add custom css support for the confirm dialog
    We can specify a custom style with {contents folder}/custom-css/confirm.css.

--- src/web/confirm.js
+++ src/web/confirm.js
@@ -53,6 +53,23 @@ Office.onReady(() => {
   );
 });
 
+async function loadCustomCssIfExists() {
+  try {
+    const path = "custom-css/confirm.css";
+    const res = await fetch(path, { method: "HEAD" });
+    if (!res.ok) {
+      console.debug("No custom CSS found, skipping loading custom-css/confirm.css");
+      return;
+    }
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = path;
+    document.head.appendChild(link);
+  } catch (error) {
+    console.debug("Failed to load custom CSS.", error);
+  }
+}
+
 let counter = 0;
 function generateTempId() {
   return `fcm_temp_${counter++}_${Date.now()}`;
@@ -190,6 +207,7 @@ function appendCheckbox({ container, id, label, warning, emphasize }) {
 async function onMessageFromParent(arg) {
   const receivedData = JSON.parse(arg.message);
   console.log(receivedData);
+  await loadCustomCssIfExists();
   const data = new ConfirmData(receivedData);
   await Promise.all([
     l10n.ready,
